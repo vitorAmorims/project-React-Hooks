85f5a117c3b55ba609db969379c16fbbe00b5ee1
vitorAmorims <vtamorims@gmail.com>
2020-12-28 17:21:38 -0300

    hook useState2 desafio Par ou Impar

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,44 +1,59 @@
 import React, { useEffect, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
-import SectionTitle from '../../components/layout/SectionTitle';
+import SectionTitle from "../../components/layout/SectionTitle";
 
 function calcFatorial(n) {
-    if (n < 0) return -1;
-    if (n === 0) return 1;
-    return calcFatorial(n - 1) * n;
+  if (n < 0) return -1;
+  if (n === 0) return 1;
+  return calcFatorial(n - 1) * n;
+}
+
+function parOuImpar(n) {
+  if (n % 2 === 0) {
+    return "Par";
+  } else {
+    return "Impar";
+  }
 }
 
 const UseEffect = (props) => {
   const [number, setNumber] = useState(1);
   const [fatorial, setFatorial] = useState(1);
-//   setFatorial(calcFatorial(number)); error: Too many re-renders. React limits the number of renders to prevent an infinite loop.
+  const [status, setStatus] = useState('impar');
+
+  //   setFatorial(calcFatorial(number)); error: Too many re-renders. React limits the number of renders to prevent an infinite loop.
   useEffect(() => {
-    setFatorial(calcFatorial(number))
+    setFatorial(calcFatorial(number));
   }, [number]);
-  
+
   useEffect(() => {
-    if (fatorial > 500) document.title = "Fatorial maior que 500"
-  }, [fatorial])
-//   function fatorial(number) {
-//     let num = number;
-//     let fatorial = 1;
-//     while (num > 0) {
-//       fatorial *= num;
-//       num = num - 1;
-//     }
-//     return fatorial;
-//   }
+    if (fatorial > 500) document.title = "Fatorial maior que 500";
+  }, [fatorial]);
+  //   function fatorial(number) {
+  //     let num = number;
+  //     let fatorial = 1;
+  //     while (num > 0) {
+  //       fatorial *= num;
+  //       num = num - 1;
+  //     }
+  //     return fatorial;
+  //   }
+  useEffect(() => {
+    setStatus(parOuImpar(number));
+  },[number]);
   return (
     <div className="UseEffect">
       <PageTitle
         title="Hook UseEffect"
         subtitle="Permite executar efeitos colaterais em componentes funcionais!"
       />
-      <SectionTitle />
+      <SectionTitle title="Exercício #01" />
       <div className="center">
         <div>
           <span className="text">Fatorial: </span>
-          <span className="text red">{fatorial === -1 ? 'Não existe' : fatorial}</span>
+          <span className="text red">
+            {fatorial === -1 ? "Não existe" : fatorial}
+          </span>
         </div>
         <input
           value={number}
@@ -53,6 +68,13 @@ const UseEffect = (props) => {
           <button onClick={() => setNumber(number - 1)} className="btn">
             -
           </button>
+        </div>
+      </div>
+      <SectionTitle title="Exercício #02" />
+      <div className="center">
+        <div>
+          <span className="text">Status input - </span>
+          <span className="text red">{status}</span>
         </div>
       </div>
     </div>
