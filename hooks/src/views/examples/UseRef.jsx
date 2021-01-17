242144b4a5eac0cd40487f77284c29419f9cd48c
vitorAmorims <vtamorims@gmail.com>
2021-01-17 17:21:35 -0300

    Exercicio 2 useRef

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,24 +2,43 @@ import React, { useEffect, useRef, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
+// fn para ex2
+function merge(s1, s2) {
+  return [...s1].map(function(letra, index) {
+    return `${letra}${s2[index] || ""}`;
+  }).join('');
+}
+
 const UseRef = (props) => {
   const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+
   const count = useRef(0)
-  console.log(count)
+  const myInput1 = useRef(null);
+  const myInput2 = useRef(null);
+
   useEffect(() => {
       count.current = count.current + 1;
-  }, [value1])
+      myInput2.current.focus();
+  }, [value1]);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+    myInput1.current.focus();
+  }, [value2]);
+
+  
   return (
     <div className="UseRef">
       <PageTitle
         title="Hook UseRef"
-        subtitle="Retorna um objeto mutável com a propriedade .current!"
+        subtitle="Retorna um objeto mutável com a chave .current!"
       />
       <SectionTitle title="Exercício #01" />
       <div className="center">
         <div>
           <span className="text">Valor: </span>
-          <span className="text">{value1} [</span>
+          <span className="text">{merge(value1, value2)} [</span>
           <span className="text red">{count.current}</span>
           <span className="text">]</span>
         </div>
@@ -27,11 +46,19 @@ const UseRef = (props) => {
         <input
           value={value1}
           onChange={(e) => setValue1(e.target.value)}
+          ref={myInput1}
           type="text"
           className="input"
         />
       </div>
       <SectionTitle title="Exercício #02" />
+      <input
+          value={value2}
+          onChange={(e) => setValue2(e.target.value)}
+          ref={myInput2}
+          type="text"
+          className="input"
+        />
     </div>
   );
 };
