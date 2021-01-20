0068c4938cf6fd1c1d4ec83a51b109e1ada9fb24
vitorAmorims <vtamorims@gmail.com>
2021-01-20 18:36:26 -0300

    custom Hooks

--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle';
 import SectionTitle from "../../components/layout/SectionTitle";
 import useCounter from '../../hooks/UseCounter';
+import {useFetch} from '../../hooks/useFetch';
 
 const UseRef = (props) => {
     const [count, inc, dec, updateState] = useCounter(); //se não quiser determinado valor [count,,]
+    
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json';
+    const response = useFetch(url);
+    const { data } = response;
+    const show = (data) => {
+      return data.map(state => <li key={state.nome}> {`${state.nome} - ${state.sigla}`} </li>)
+    };
+
     return (
       <div className="UseCustom">
         <PageTitle
@@ -13,6 +22,7 @@ const UseRef = (props) => {
         />
         <SectionTitle title="Exercício #01" />
         <div className="center">
+          <h3>Usando custom Hooks useCounter</h3>
           <span className="text">{count}</span>
           <div className="center">
             <button className="btn" onClick={() => inc()}>
@@ -27,6 +37,13 @@ const UseRef = (props) => {
               placeholder="alterar numero inicial"
             />
           </div>
+        </div>
+        <SectionTitle title="Exercício #02" />
+        <div className="center">
+          <h3>Usando custom Hooks useFetch</h3>
+        <ul>
+          {!response.loading ? show(response.data) : false }
+        </ul>
         </div>
       </div>
     );
